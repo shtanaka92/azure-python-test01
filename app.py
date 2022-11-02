14380f9984ccfa60bf6a5407a49861f4a93b9d66
shtanaka92 <92288913+shtanaka92@users.noreply.github.com>
2022-11-02 15:34:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     import requests
-from datetime import datetime
-import re
-import pandas as pd
-from bs4 import BeautifulSoup
+    import pandas as pd
+    from bs4 import BeautifulSoup
     return "Hello, World!"
