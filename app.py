b870211c2afea5e5d2e621d4c17d5ce86c5598d3
shtanaka92 <92288913+shtanaka92@users.noreply.github.com>
2022-11-02 16:52:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template # 追加
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello World3"
+    return render_template('index.html') # 変更
 
 if __name__ == "__main__":
     app.run(debug=True)
