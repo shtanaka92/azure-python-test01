a134ebe285be6c573bd6b5d7b110bbef60a84472
shtanaka92 <92288913+shtanaka92@users.noreply.github.com>
2022-11-02 16:09:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html') # 変更
+    return "hello World2"
 
 if __name__ == "__main__":
     app.run(debug=True)
