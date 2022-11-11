6f23288525c5539a7e5bab60969788df7383425e
shtanaka92 <92288913+shtanaka92@users.noreply.github.com>
2022-11-11 17:59:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', message="Python練習")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/result', methods=["GET"])
+def result_get():
+    year = request.args.get("year","")
+    corse = request.args.get("corse","")
+    number = request.args.get("number","")
+    day = request.args.get("day","")
+    race = request.args.get("race","")
+    url = 'https://race.netkeiba.com/race/shutuba.html?race_id='+ year + corse + number + day + race
+
+    return render_template('result.html', message = url)
+
+if __name__ == '__main__':
+    app.debug = False
+    app.run(host='localhost')
