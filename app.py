d17700693b53f881688a29fe925f91f319708657
shtanaka92 <92288913+shtanaka92@users.noreply.github.com>
2022-11-15 15:18:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template,request
+import requests
+from datetime import datetime
+import re
+import pandas as pd
+from bs4 import BeautifulSoup
+name = []
+odds = []
 
 app = Flask(__name__)
 
@@ -13,9 +20,20 @@ def result_get():
     number = request.args.get("number","")
     day = request.args.get("day","")
     race = request.args.get("race","")
-    url = 'https://race.netkeiba.com/race/shutuba.html?race_id='+ year + corse + number + day + race
+    url = 'https://db.netkeiba.com/race/'+ year + corse + number + day + race
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content,'lxml')
+    t = datetime.now().strftime("%Y/%m/%d %H:%M")
 
-    return render_template('result.html', message = url)
+    for i in soup.find_all(href=re.compile("/horse/")):
+        name.append(i.string)
+
+    for o in soup.find_all(class_="txt_r",nowrap="nowrap")[3::5]:
+        odds.append(o.string)
+
+    df = pd.DataFrame({"name":name,t:odds})
+
+    return render_template('result.html', message = df)
 
 if __name__ == '__main__':
     app.debug = False
